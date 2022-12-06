2c7824aeb01e1fc730486ded6bebddd2c0c40f4e
Venkata Teja <venkatateja.thokala@gmail.com>
2022-12-05 22:57:29 -0500

    First commit

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const {MongoClient} = require('mongodb');
 const http = require('http');
-const url = "mongodb+srv://Venkatateja_thokala:Teja1234@cluster0.ilvuzg9.mongodb.net/?retryWrites=true&w=majority";
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const mongourl = "mongodb+srv://Venkatateja_thokala:Teja1234@cluster0.ilvuzg9.mongodb.net/?retryWrites=true&w=majority";
 
 const host = 'localhost';
 const port = 9000;
 
 const requestListener = async function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" });
-    res.write(JSON.stringify(await dbClient()));
-    res.end();
+    if(req.url === '/about') {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        fs.readFile(path.join(__dirname, 'public','index.html'), (err, data) =>{
+            if(err) throw err;
+            res.writeHead(200, {'content-Type':'text/html'});
+            res.end(data);
+        })
+    } else if(req.url === '/api') {
+        fs.readFile(path.join(__dirname, '','db.json'), (err, data) =>{
+            if(err) throw err;
+            res.writeHead(200, {'content-Type':'application/json'});
+            res.end(data);
+        })
+    } else {
+        const queryObject = url.parse(req.url, true).query;
+        res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" });
+        res.write(JSON.stringify(await dbClient(queryObject)));
+        res.end();
+    }
 };
 
 const server = http.createServer(requestListener);
@@ -16,25 +35,25 @@ server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
 
-async function dbClient() {
-    const client = new MongoClient(url);
-    const list = await getHospitalsListing(client);
+async function dbClient(query) {
+    const client = new MongoClient(mongourl);
+    const list = await getHospitalsListing(client, query);
     return list;
 }
 
 
-async function getHospitalsListing(client) {
+async function getHospitalsListing(client, query) {
     var hospitalsDetails = null;
     try {
         console.log('TEJA');
         client.connect();
-        const query = { state: "CT" };
+       // const query = { state: "CT" };
         hospitalsDetails = await client.db("Hospital_Listing").collection("Hospital_Details").find(query).toArray();
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
     }
-    console.log(hospitalsDetails);
+    //console.log(hospitalsDetails);
     return hospitalsDetails;
 }
